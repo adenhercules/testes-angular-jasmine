f6b68c005f54b5bb4d2ed90744ad0086660ff587
adenhercules <adenhercules07@gmail.com>
2023-01-06 01:39:30 -0400

    elegancia nos testes

--- src/app/shared/service/unique-id/unique-id.service.spec.ts
+++ src/app/shared/service/unique-id/unique-id.service.spec.ts
@@ -4,7 +4,16 @@ describe(UniqueIdService.name, () => { // não passar o nome da classe como stri
   it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generatedUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue(); // dando mais precisão no prefixo app
+  });
+
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should not generated duplicate IDs when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 50; i++){
+      ids.add(service.generatedUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
   });
 });
 
