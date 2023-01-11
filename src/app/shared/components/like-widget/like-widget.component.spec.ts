73348755e9c4a5b20b181f64ceae1cfd105d937d
adenhercules <adenhercules07@gmail.com>
2023-01-11 01:31:29 -0400

    removendo imports

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,7 +1,5 @@
 import {LikeWidgetComponent} from "./like-widget.component";
-import {UniqueIdService} from "../../service/unique-id/unique-id.service";
 import {TestBed} from "@angular/core/testing";
-import {LikeWidgetModule} from "./like-widget.module";
 
 describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent = null;
