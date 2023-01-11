09a04dfae8d5e3c682bc198ee3041b891cb07b59
adenhercules <adenhercules07@gmail.com>
2023-01-11 01:38:16 -0400

    testandop components

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,16 +1,24 @@
 import {LikeWidgetComponent} from "./like-widget.component";
-import {TestBed} from "@angular/core/testing";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {UniqueIdService} from "../../service/unique-id/unique-id.service";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 describe(LikeWidgetComponent.name, () => {
-  let component: LikeWidgetComponent = null;
+  let fixture: ComponentFixture <LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent]
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule]
     }).compileComponents();
 
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+
   });
-  it('', () => {
+  it('Should creat component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
 
   });
 });
